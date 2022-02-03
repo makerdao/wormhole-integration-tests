14ac10f643dd101b9a5e853dd9481c0848676992
Krzysztof Kaczor <krzkaczor@gmail.com>
2022-02-03 15:40:18 +0100

    Fix CI issues

--- scripts/build-infra.sh
+++ scripts/build-infra.sh
@@ -2,6 +2,8 @@
 set -e
 cd "$(dirname "$0")"
 
+cp -f ./ops/Dockerfile ../repos/optimism-monorepo/ops/docker/hardhat/Dockerfile
+cp -f ./ops/yarn.lock ../repos/optimism-monorepo/ops/docker/hardhat/yarn.lock
 cd ../repos/optimism-monorepo/ops
 
 docker-compose build
