01019fdbbce21878cb7b055bb13689edc23de342
olivdb <olivdb@protonmail.com>
2022-02-04 22:02:58 +0000

    Add whitespace

--- test/wormhole.arbitrum.test.ts
+++ test/wormhole.arbitrum.test.ts
@@ -3,4 +3,5 @@ import { setupArbitrumTests } from './arbitrum'
 import { runWormholeTests } from './wormhole.test'
 
 const arbitrumDomain = ethers.utils.formatBytes32String('ARBITRUM-A')
+
 runWormholeTests(arbitrumDomain, setupArbitrumTests)
