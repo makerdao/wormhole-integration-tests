aafd7bc3c21858aa0f998e28197a389f5f1e8704
olivdb <olivdb@protonmail.com>
2022-03-04 11:04:21 +0000

    Add etherscan verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv'
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
 
 dotenv.config()
 
@@ -17,9 +18,18 @@ const config: HardhatUserConfig = {
       // prevents gas estimations problems
       gas: 'auto',
     },
+    kovan: {
+      url: process.env.KOVAN_OPTIMISM_L1_RPC,
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_ARBITRUM_L1_RPC,
+    },
   },
   mocha: {
     timeout: 5000_000,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
   },
 }
 
